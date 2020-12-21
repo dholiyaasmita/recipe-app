fe28fb6060e3b71d4d37898b055fc434d9e4e5a7
Asmita Dholiya <asmi.katrodiya@gmail.com>
2020-12-21 11:10:15 -0500

    Added data into the table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
-Insert into category (description) value ('Indian');
-Insert into category (description) value ('American');
-Insert into category (description) value ('Italian');
-Insert into category (description) value ('Mexican');
-Insert into category (description) value ('Canadian');
+Insert into category (description) values ('Indian');
+Insert into category (description) values ('American');
+Insert into category (description) values ('Italian');
+Insert into category (description) values ('Mexican');
+Insert into category (description) values ('Canadian');
 
-Insert into unit_of_measure (description) value ('Teaspoon');
-Insert into unit_of_measure (description) value ('Tablespoon');
-Insert into unit_of_measure (description) value ('Cup');
-Insert into unit_of_measure (description) value ('Pinch');
-Insert into unit_of_measure (description) value ('Ounce');
+Insert into unit_of_measure (description) values ('Teaspoon');
+Insert into unit_of_measure (description) values ('Tablespoon');
+Insert into unit_of_measure (description) values ('Cup');
+Insert into unit_of_measure (description) values ('Pinch');
+Insert into unit_of_measure (description) values ('Ounce');
